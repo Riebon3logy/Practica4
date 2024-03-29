eb906b6d509f729c4ac503c4ee0ad0d1f43dfdd3
Andres Le Gresley <162372913+Riebon3logy@users.noreply.github.com>
2024-03-28 22:47:36 -0700

    Add files via upload

---
+++ Baraja.java
@@ -0,0 +1,46 @@
+package com.mycompany.practica04;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * @author Andres Ferrel
+ * 
+ * Esta clase crea una baraja usando ArrayList de tipo carta para poder crear
+ * todas las cartas necesarias que componen una baraja española.
+ * 
+ * Version (1.0)
+ * 
+ **/
+
+public class Baraja {
+
+    private ArrayList<CartaLogica> baraja;
+
+    public Baraja() {
+        baraja = new ArrayList<>();
+        String[] suits = {"Oros", "Copas", "Espadas", "Bastos"};
+        String[] values = {"As", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Sota", "Caballo", "Rey"};
+
+        for (String suit : suits) {
+            for (String value : values) {
+                baraja.add(new CartaLogica(suit, value));
+            }
+        }
+    }
+    
+    //Metodo para mezclar las cartas
+    public void mezclar() {
+        Collections.shuffle(baraja);
+    }
+    
+    //Metodo para poder disponer de las cartas
+    public ArrayList<CartaLogica> getBaraja() {
+        return baraja;
+    }
+
+    // Imprimir las cartas en la baraja usando lambda
+    public void imprimirCartas () {
+        getBaraja().forEach(CartaLogica -> System.out.println(CartaLogica));
+    }
+}
---
+++ CartaLogica.java
@@ -0,0 +1,49 @@
+package com.mycompany.practica04;
+
+/**
+ * @author Andres Le Gresley
+ * 
+ * Esta clase sirve para modelar una carta de baraja española la cual podra 
+ * ser vista desde la terminal de java.
+ * 
+ * Version (1.0)
+ * 
+ **/
+
+public class CartaLogica {
+    
+    //Atributos privados de la clase para poder crear la carta
+    private String suit; // Palos: "Oros", "Copas", "Espadas", "Bastos"
+    private String value; // Valores: "As", "2", "3", ..., "10", "Sota", 
+                          // "Caballo", "Rey"
+    
+    //Constructor de la carta
+    public CartaLogica(String suit, String value) {
+        this.suit = suit;
+        this.value = value;
+    }
+    
+    //Getters de la clase
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getValue() {
+        return value;
+    }
+    
+    //Setters de la clase
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+    
+    //Metodo toString sobreescrito
+    @Override
+    public String toString() {
+        return value + " de " + suit;
+    }
+}
