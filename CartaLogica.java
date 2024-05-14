260a427c21b5eb36fc6dfa69044803cf968273eb
Andres Le Gresley <162372913+Riebon3logy@users.noreply.github.com>
2024-05-13 21:43:42 -0700

    Update CartaLogica.java

--- CartaLogica.java
+++ CartaLogica.java
@@ -1,29 +1,30 @@
 package com.mycompany.practica04;
 
 /**
- * 
+ *
  * @authors Andres Le Gresley - Paul Chavez
- * 
- * Esta clase sirve para modelar una carta de baraja española la cual podra 
- * ser vista desde la terminal de java.
- * 
+ *
+ * Esta clase sirve para modelar una carta de baraja española la cual podra ser
+ * vista desde la terminal de java.
+ *
  * Version (1.0)
- * 
- **/
-
+ *
+ *
+ *
+ */
 public class CartaLogica {
-    
+
     //Atributos privados de la clase para poder crear la carta
     private String suit; // Palos: "Oros", "Copas", "Espadas", "Bastos"
     private String value; // Valores: "As", "2", "3", ..., "10", "Sota", 
-                          // "Caballo", "Rey"
-    
+    // "Caballo", "Rey"
+
     //Constructor de la carta
     public CartaLogica(String suit, String value) {
         this.suit = suit;
         this.value = value;
     }
-    
+
     //Getters de la clase
     public String getSuit() {
         return suit;
@@ -32,7 +33,7 @@ public class CartaLogica {
     public String getValue() {
         return value;
     }
-    
+
     //Setters de la clase
     public void setSuit(String suit) {
         this.suit = suit;
@@ -41,7 +42,7 @@ public class CartaLogica {
     public void setValue(String value) {
         this.value = value;
     }
-    
+
     //Metodo toString sobreescrito
     @Override
     public String toString() {
