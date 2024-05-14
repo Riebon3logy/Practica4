f2407f95cdc91f9a27924b7d3f4374fefbf63bcd
Andres Le Gresley <162372913+Riebon3logy@users.noreply.github.com>
2024-05-13 21:43:11 -0700

    Update MesaDeJuego.java

--- MesaDeJuego.java
+++ MesaDeJuego.java
@@ -5,49 +5,71 @@ import java.util.ArrayList;
 /**
  *
  * @authors Andres Le Gresley - Paul Chavez
- * 
+ *
  * Esta clase modela la mesa de juego en la cual los jugadores podran colocar
  * las cartas que se vayan jugando
- * 
+ *
  * Version (1.0)
- * 
- **/
-
+ *
+ *
+ *
+ */
 public class MesaDeJuego {
-    
+
     //Atributos de la clase
     private ArrayList<CartaLogica> cartasEnMesa;
-    private Baraja baraja;
 
     //Constructor de la mesa de juego
-    public MesaDeJuego(Baraja baraja) {
-        this.cartasEnMesa = new ArrayList<>(); //ArrayList de cartas jugadas
-        this.baraja = baraja;
+    public MesaDeJuego() {
+        cartasEnMesa = new ArrayList<>(); //ArrayList de cartas jugadas
     }
-    
+
     //Coloca el objeto carta y lo añade al ArrayList
-    public void colocarCarta(Jugador jugador, int indice) {
-        CartaLogica carta = jugador.colocarCarta(indice);
-        if (carta != null) {
-            cartasEnMesa.add(carta);
-            System.out.println(jugador.getNombre() + " coloca la carta: " + carta);
-        }
+    public void agregarCartaAMesa(CartaLogica carta) {
+        cartasEnMesa.add(carta);
     }
-    
+
     //Imprime las cartas que se han jugado en la terminal
     public void mostrarCartasEnMesa() {
-        System.out.println("Cartas en la mesa:");
         for (CartaLogica carta : cartasEnMesa) {
             System.out.println(carta);
         }
     }
-    
-    //Reparte cartas en funcion de la cantidad de jugadores
-    public void repartirCartas(ArrayList<Jugador> jugadores, int cantidadCartasPorJugador) {
-        for (Jugador jugador : jugadores) {
-            for (int i = 0; i < cantidadCartasPorJugador; i++) {
-                jugador.tomarCartaDeBaraja(baraja);
-            }
+
+    // Metodo booleano para saber si la mesa esta vacia
+    public boolean estaVaciaMesa() {
+        return cartasEnMesa.isEmpty();
+    }
+
+    // Metodo de acceso a la ultima carta de la mesa
+    public CartaLogica getUltimaCarta() {
+        if (!estaVaciaMesa()) {
+            return cartasEnMesa.get(cartasEnMesa.size() - 1);
+        } else {
+            return null;
         }
+    }
+
+    // Metodo para agregar una carta en la mesa
+    public CartaLogica colocarCarta() {
+        if (!estaVaciaMesa()) {
+            return cartasEnMesa.remove(0);
+        } else {
+            return null;
+        }
+    }
+
+    // Metodo para colocar la ultima carta de la mesa en algun lado
+    public CartaLogica colocarUltimaCarta() {
+        if (!estaVaciaMesa()) {
+            return cartasEnMesa.remove(cartasEnMesa.size() - 1);
+        } else {
+            return null;
+        }
+    }
+
+    // Metodo de acceso al ArrayList de cartas que hay en la mesa
+    public ArrayList<CartaLogica> getMesa() {
+        return cartasEnMesa;
     }
 }
